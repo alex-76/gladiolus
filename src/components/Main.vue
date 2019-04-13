c878267c0a45bff0d479c34aacb0542c9cbc2fbe
Alex <alex0676@gmail.com>
2019-04-13 12:30:34 +0300

    edit VueJS

---
+++ src/components/Main.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="hello">
+        <h1>{{ msg }}</h1>
+        <h1><span>Group Alex...</span></h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Main',
+        data () {
+            return {
+                msg: 'Welcome Alex'
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+    h1 {
+        color: green;
+        span {
+           color:red;
+        }
+    }
+</style>
