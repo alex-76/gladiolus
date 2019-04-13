ff56748a3cb883b4481723a0f3788ad0757864df
Alex76 <alex0676@gmail.com>
2019-04-13 09:35:47 +0300

    init vueJS

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,25 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Main from '@/components/Main'
+import Form from '@/components/Form'
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
-})
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'Main',
+            component: Main,
+            props: true
+        },
+        {
+            path: '/form',
+            name: 'Form',
+            component: Form,
+            props: true
+        }
+    ]
+});
+
